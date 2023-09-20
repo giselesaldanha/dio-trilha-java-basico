1d408f052bd2b21980c87f416182d37f2e08bc8a
giselesaldanha <giselesaldanha@yahoo.com.br>
2023-09-20 11:48:08 -0300

    Tipos de Variáveis Primárias do Java

--- TIPO-VARIAVEIS/src/TiposVariaveis.java
+++ TIPO-VARIAVEIS/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //Variável do Tipo Texto
+        String nome="Gisele Saldanha";
+
+        //Variáveis Numéricas
+        //O tipo long precisa terminar com L, o tipo float precisa terminar com F
+        byte idade = 123;
+		short ano = 2021;
+		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo, por exemplo string
+		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo, por exemplo string
+		float pi = 3.14F; 
+		double salario = 1275.33;
+
+        //Constante deve ser declarada com todas as letras em maiúsculo e deve ser escrito a palavra "final" no início da declaração da variável.
+        final double VALOR_DE_PI = 3.14;
+		VALOR_DE_PI=3.15;
     }
 }
